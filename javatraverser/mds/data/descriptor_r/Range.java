b0033142f6f7d7a50613d57dc91ed094b3dbbf18
zack-vii <timo.schroeder@ipp.mpg.de>
2016-06-14 14:24:42 +0200

    handle Range

--- javatraverser/mds/data/descriptor_r/Range.java
+++ javatraverser/mds/data/descriptor_r/Range.java
@@ -18,6 +18,13 @@ public final class Range extends Descriptor_R{
         this.dscptrs[2] = delta;
     }
 
+    @Override
+    public final StringBuilder decompile(final int prec, final StringBuilder pout) throws MdsException {
+        pout.append(DTYPE.getName(DTYPE.RANGE));
+        this.addArguments(0, "(", ")", pout);
+        return pout;
+    }
+
     public final Descriptor getBegin() {
         return this.dscptrs[0];
     }
