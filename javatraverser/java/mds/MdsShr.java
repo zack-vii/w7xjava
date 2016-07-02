06cf525c524a21f6f24d5a1b9fe11fa06004cc91
zack-vii <timo.schroeder@ipp.mpg.de>
2016-07-02 13:54:44 +0200

    MdsShr

--- javatraverser/java/mds/MdsShr.java
+++ javatraverser/java/mds/MdsShr.java
@@ -1,5 +1,7 @@
 package mds;
 
+import mds.data.descriptor.Descriptor;
+import mds.data.descriptor_a.Int8Array;
 import mds.mdsip.Connection;
 
 public final class MdsShr{
@@ -10,10 +12,19 @@ public final class MdsShr{
     }
 
     public final int mdsEvent(final String event) throws MdsException {
-        return this.connection.getInteger(String.format("MdsShr->MDSEvent(ref('%s'),val(0),val(0))", event));
+        return this.connection.getInteger(String.format("_s=MdsShr->MDSEvent(ref('%s'),val(0),val(0))", event));
     }
 
     public final String mdsGetMsgDsc(final int status) throws MdsException {
         return this.connection.getString(String.format("_a=repeat(' ',256);MdsShr->MdsGetMsgDsc(val(%d),descr(_a));trim(_a)", status));
+    }
+
+    public final Descriptor mdsSerializeDscIn(final Int8Array serial) throws MdsException {
+        return this.connection.mdsValue("_a=*;_s=MdsShr->MdsSerializeDscIn(ref($),xd(_a));_a", new Descriptor[]{serial}, Descriptor.class);
+    }
+
+    public final Int8Array mdsSerializeDscOut(final String expr) throws MdsException {
+        return (Int8Array)this.connection.mdsValue(new StringBuffer(expr.length() + 64)//
+        .append("_a=*;_s=MdsShr->MdsSerializeDscOut(xd((").append(expr).append(";)),xd(_a));_a").toString(), Int8Array.class);
     }
 }
---
+++ javatraverser/junit/mds/MdsShr_Test.java
@@ -0,0 +1,52 @@
+package mds;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import mds.data.descriptor.Descriptor;
+import mds.data.descriptor_a.Int8Array;
+import mds.mdsip.Connection;
+
+@SuppressWarnings("static-method")
+public class MdsShr_Test{
+    private static Connection mds;
+    private static MdsShr     mdsshr;
+
+    @BeforeClass
+    public static final void setUpBeforeClass() throws Exception {
+        MdsShr_Test.mds = AllTests.setUpBeforeClass();
+        MdsShr_Test.mdsshr = new MdsShr(MdsShr_Test.mds);
+    }
+
+    @AfterClass
+    public static final void tearDownAfterClass() throws Exception {
+        MdsShr_Test.mds.disconnect();
+    }
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @After
+    public void tearDown() throws Exception {}
+
+    @Test
+    public final void testMdsEvent() throws MdsException {
+        Assert.assertEquals(1, MdsShr_Test.mdsshr.mdsEvent("myevent"));
+    }
+
+    @Test
+    public final void testMdsGetMsgDsc() throws MdsException {
+        Assert.assertEquals("%SS-S-SUCCESS, Success", MdsShr_Test.mdsshr.mdsGetMsgDsc(1));
+    }
+
+    @Test
+    public final void testMdsSerializeDsc() throws MdsException {
+        final String result = "Build_With_Units(101325., \"Pa\")";
+        final Int8Array serial = MdsShr_Test.mdsshr.mdsSerializeDscOut("$P0");
+        Assert.assertEquals(result, Descriptor.deserialize(serial.getBuffer()).decompile());
+        Assert.assertEquals(result, MdsShr_Test.mdsshr.mdsSerializeDscIn(serial).decompile());
+    }
+}
