56093905027073662ae0fb44481bf0423e767005
zack-vii <zack.vii@live.com>
2016-07-18 21:07:30 +0200

    fixed FLOAT decompile

--- javatraverser/java/mds/data/descriptor_s/FLOAT.java
+++ javatraverser/java/mds/data/descriptor_s/FLOAT.java
@@ -3,22 +3,23 @@ package mds.data.descriptor_s;
 import java.nio.ByteBuffer;
 import java.util.Locale;
 import mds.data.descriptor.DTYPE;
-import mds.data.descriptor.Descriptor;
 
 public abstract class FLOAT<T extends Number>extends NUMBER<T>{
     public static final String decompile(final Number value, final byte dtype, final int mode) {
-        final boolean preview = (mode & Descriptor.DECO_STR) != 0;
-        String tmp = String.format(Locale.US, "%s", value);
-        if(preview && tmp.length() > 5) tmp = String.format(Locale.US, "%1.3G", value);
-        tmp.replace("+", "");
-        if(DTYPE.F == dtype || DTYPE.FS == dtype){
-            if(value.equals(0.0f)) return "0.";
-            if(tmp.endsWith("E0")) tmp = tmp.substring(0, tmp.length() - 2);
-            else if(tmp.contains("E")) return tmp.replaceAll("^0*", "");
-            return tmp.replaceAll("(^0*|0*$)", "");
+        final boolean isF = dtype == DTYPE.F || dtype == DTYPE.FS;
+        if(value.doubleValue() == 0) return isF ? "0." : new StringBuilder(3).append('0').append(DTYPE.getSuffix(dtype)).append('0').toString();
+        final double val, absval = Math.abs(value.doubleValue());
+        final int E;
+        if(absval >= 1e6 || absval < .1){
+            E = (int)Math.log10(absval);
+            val = value.doubleValue() / Math.pow(10, E);
+        }else{
+            val = value.doubleValue();
+            E = 0;
         }
-        if(tmp.contains("E")) return tmp.replace("E", DTYPE.getSuffix(dtype));
-        return preview ? tmp : tmp.concat(DTYPE.getSuffix(dtype)).concat("0");
+        final String tmp = String.format(Locale.US, "%f", val);
+        if(isF && E == 0) return tmp.replaceAll("(^0*|0*$)", "");
+        return new StringBuilder(16).append(tmp.replaceAll("(^0*|[0\\.]*$)", "")).append(DTYPE.getSuffix(dtype)).append(E).toString();
     }
 
     protected FLOAT(final byte dtype, final double value){
